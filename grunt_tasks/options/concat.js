ac6a642944ab456355843bf8f06c7ff9ed51b6fc
Pixelastic <tim@pixelastic.com>
2015-02-08 23:44:53 +0100

    fix(images): Editing posts containing broken links to images.

--- grunt_tasks/options/concat.js
+++ grunt_tasks/options/concat.js
@@ -4,6 +4,7 @@ module.exports = {
   prodJsTmpToOutput: {
     src: [
       'tmp/js/src/zepto.min.js',
+      'tmp/js/src/moment.js',
       'tmp/js/src/lodash.min.js',
       'tmp/js/src/algoliasearch.min.js',
       'tmp/js/src/app.js'

a0cc33787af65b26ed4821a73ee93e6c4e7678e5
Pixelastic <tim@pixelastic.com>
2019-10-10 10:25:33 +0200

    feat(init): Add basic eleventy

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-commonjs */
+module.exports = {
+  dir: {
+    input: 'src',
+    output: 'dist',
+    layouts: '_layouts',
+  },
+};
---
+++ src/index.md
@@ -0,0 +1,7 @@
+---
+layout: default.pug
+---
+
+## Test
+
+ok, looks good

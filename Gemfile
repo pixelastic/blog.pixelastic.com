e7edf05ec10fd1b884fa059dfcbe494ba1afaf6c
Pixelastic <tim@pixelastic.com>
2019-10-10 09:29:01 +0200

    chore(clean): Tabula rasa

--- Gemfile
+++
@@ -1,9 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'jekyll', '>=2.5.3'
-gem 'i18n', '>= 0.7.0'
-
-group :jekyll_plugins do
-  gem 'algoliasearch-jekyll'
-end
-
--- grunt_tasks/algolia_task.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env node */
-
-module.exports = function(grunt) {
-  grunt.registerTask('algolia', [
-    'build',
-    'shell:jekyllAlgolia'
-  ]);
-
-};
